9475be25bdf2c28c9834b23f78f76d966fcac2fe
syskey <lopetvers@gmail.com>
2023-09-12 13:45:57 +0800

    update gemini-3f-2023-sep-11

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@
 # @twitter:https://twitter.com/skyMetaverse
 
 # update ubuntu-x86_64-v2-gemini-3f-2023-sep-05
-wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-05/subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-05
-mv subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-05 node
-wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-05/subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-05
-mv subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-05 farmer
+wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-11/subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-11
+mv subspace-node-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 node
+wget https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-11/subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11
+mv subspace-farmer-ubuntu-x86_64-v2-gemini-3f-2023-sep-11 farmer
 chmod +x node.sh farmer.sh node farmer
 ./node.sh
-sleep 10
+sleep 30
 ./farmer.sh
 
 echo "运行成功"
